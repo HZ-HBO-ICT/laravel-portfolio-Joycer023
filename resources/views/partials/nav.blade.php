fe662742b21c413e64699875fabb8355c4fb19e6
Joycer023 <roth0008@hz.nl>
2022-02-13 10:07:02 +0100

    add bol.blade.php to views

---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,29 @@
+<nav>
+    <div class="logo">Joyce Roth</div>
+    <label for="btn" class="icon">
+        <span class="fas fa-bars"></span>
+    </label>
+    <input type="checkbox" id="btn">
+    <ul>
+        <li><a href="/" >Home</a></li>
+        <li><a href="profile" >Profile</a></li>
+        <li><a href="dashboard">Dashboard</a></li>
+        <li>
+            <label for="btn-1" class="show">Profession +</label>
+            <a href="profession">Profession</a>
+            <input type="checkbox" id="btn-1">
+            <ul>
+                <li><a href="nordwave" >Nordwave Security</a></li>
+                <li><a href="capgemini">Capgemini</a></li>
+                <li><a href="centric">Centric</a></li>
+                <li><a href="ing">ING Group</a></li>
+                <li><a href="bol">Bol.com</a></li>
+                <li><a href="macaw">Macaw</a></li>
+                <li><a href="yoursurprise">YourSurprise</a></li>
+                <li><a href="veracode">VeraCode</a></li>
+            </ul>
+        </li>
+        <li><a href="motivation.html">Motivation</a></li>
+        <li><a href="faq.html">FAQ</a></li>
+    </ul>
+</nav>
