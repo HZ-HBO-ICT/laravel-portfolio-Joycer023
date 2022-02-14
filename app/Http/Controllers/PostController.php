99210855edd2fa67f323d0128b08e7b1c3c56810
Joycer023 <roth0008@hz.nl>
2022-02-14 12:41:39 +0100

    update PostController. Add database.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,21 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+
+
 class PostController extends Controller
 {
-    public function show($post)
+    public function show($slug)
     {
-        $posts = [
-            'my-first-post' => 'Hello, this is my first blog post!',
-            'my-second-post' => 'Now I am getting the hang of this blogging thing.'
-        ];
-
-        if (!array_key_exists($post, $posts)) {
-            abort(404, 'Sorry, that post was not found.');
-        }
-
         return view('post', [
-            'post' => $posts[$post]
+            'post' => Post::where('slug', $slug) -> firstOrFail()
         ]);
     }
 }
